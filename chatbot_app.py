7fb9fffd5c8979ce8fcd9c1227ea3b62e96c0e68
NicholasParrilla <161997027+NicholasParrilla@users.noreply.github.com>
2024-03-03 00:08:24 -0800

    Update chatbot_app.py
    updated a series of deprecated packages to resolve errors and now initiating the llm is error-free

--- chatbot_app.py
+++ chatbot_app.py
@@ -1,8 +1,9 @@
 from dotenv import load_dotenv
-from langchain.embeddings.openai import OpenAIEmbeddings
-from langchain.llms import OpenAI
+from langchain_openai import OpenAIEmbeddings, ChatOpenAI
+from langchain_community.llms import OpenAI
 from langchain_pinecone import PineconeVectorStore
+
 
 load_dotenv()
 vector_store = PineconeVectorStore(index_name="starter-index", embedding=OpenAIEmbeddings())
-llm = OpenAI(model_name="gpt-3.5-turbo", temperature=0)
+llm = ChatOpenAI(model_name="gpt-3.5-turbo", temperature=0)
