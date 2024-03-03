943f8f607d148adeb7bba916a285288de0a980d3
Elliot Meldrum <91166747+meldrum2718@users.noreply.github.com>
2024-03-03 00:46:52 -0800

    first pass at loading a doc into a vector store (#10)

--- upsert_index.py
+++ upsert_index.py
@@ -1,28 +1,42 @@
 from os import getenv
+import argparse
+
 
 from dotenv import load_dotenv
-from langchain.text_splitter import CharacterTextSplitter
 from langchain_community.document_loaders import TextLoader
 from langchain_openai import OpenAIEmbeddings
+from langchain.text_splitter import CharacterTextSplitter
 from pinecone import Pinecone
 
-load_dotenv()
+def main(fname, separator):
+    load_dotenv()
 
-# load the documents and split them.
-# This can be substituted with any data type loader according to your requirement.
-loader = TextLoader("./savings-accounts.txt")
-documents = loader.load()
+    loader = TextLoader('./data/SB34.txt')
+    documents = loader.load()
 
-# We proceed by segmenting the documents and generating their embeddings.
-text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
-docs = text_splitter.split_documents(documents)
-embeddings = OpenAIEmbeddings().embed_documents(docs)
+    # TODO adopt a better chunking strategy
+    text_splitter = CharacterTextSplitter(separator="\n\n", chunk_size=100, chunk_overlap=10)
+    docs = text_splitter.split_documents(documents)
 
-# initialize pinecone
-# get the api key and environment from the .env file
-# pc = Pinecone(api_key=getenv("PINECONE_API_KEY"))
-# pc_index = pc.Index(name="starter-index")
-# pc_index.upsert(items=[{"id": doc, "vector": embed} for doc, embed in zip(docs, embeddings)])
+    strings = [doc.dict()['page_content'] for doc in docs]
+    embeddings = OpenAIEmbeddings().embed_documents(strings)
 
-if __name__ == "__main__":
-    print(embeddings)
+    vectors = [
+        {"id": str(idx), "values": embeddings[idx]}
+        for idx in range(len(embeddings))
+    ]
+
+    pc = Pinecone(api_key=getenv("PINECONE_API_KEY"))
+    pc_index = pc.Index(name="starter-index")
+    pc_index.upsert(vectors=vectors)
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-f', '--filename', type=str)
+    parser.add_argument('--separator', type=str)
+    args = parser.parse_args()
+
+    separator = args.separator if args.separator is not None else '\n\n'
+    fname = args.filename
+    main(fname, separator)
